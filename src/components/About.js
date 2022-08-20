2a465c37bd3aed2d953c11b4d556cf7463b2d406
tuanhoangle <tuanleprofessional@gmail.com>
2022-08-20 00:12:32 -0400

    about page 2nd half

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,36 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, ImageList, ImageListItem } from "@mui/material";
 import aboutmeimage from "../images/webdeveloper.jpeg";
+import basketball_gif from "../images/basketball_gif.gif";
+import coding_gif from "../images/coding_gif.gif";
+import ramen_gif from "../images/ramen_gif.gif";
+import workout_gif from "../images/workout_gif.gif";
+
+import {
+  backgroundColor1,
+  backgroundColor2,
+  textColor1,
+  textColor2,
+} from "./ColorScheme";
+
+const imagesData = [
+  {
+    img: basketball_gif,
+    title: "basketball gif",
+  },
+  {
+    img: coding_gif,
+    title: "coding gif",
+  },
+  {
+    img: ramen_gif,
+    title: "eating ramen gif",
+  },
+  {
+    img: workout_gif,
+    title: "workout gif",
+  },
+];
 
 export default function About() {
   return (
@@ -9,16 +39,19 @@ export default function About() {
       id="About"
       sx={{
         flex: 1,
+        backgroundColor: backgroundColor2,
+        text: textColor2,
       }}
     >
-      <Grid item xs={12} sm={6} sx={{ padding: 10 }}>
+      <Grid item xs={12} sm={6} sx={{ padding: 10, textAlign: "center" }}>
         <img src={aboutmeimage} alt="Web developer" />
       </Grid>
       <Grid item xs={12} sm={6} sx={{}}>
-        <Box sx={{ width: "80%", padding: 5 }}>
-          <Typography variant="h4">Roses Are Red</Typography>
-          <Typography variant="h4">Violet Are Blue,</Typography>
-          <Typography variant="h4">Unexpected {"'{'"} On Line 32</Typography>
+        <Box sx={{ width: "80%", padding: 5, textAlign: "left" }}>
+          <Typography variant="h3">Roses Are Red</Typography>
+          <Typography variant="h3">Violet Are Blue,</Typography>
+          <Typography variant="h3">Unexpected {"'{'"}</Typography>
+          <Typography variant="h3">On Line 32</Typography>
           <Typography variant="subtitle1" sx={{ paddingTop: 2 }}>
             I am a detail-oriented undergraduate who like to be as efficient as
             possible when creating a working reliable software. Whether working
@@ -26,6 +59,34 @@ export default function About() {
             problem-solving, teamwork and communications skills.
           </Typography>
         </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{}}>
+        <Box sx={{ width: "80%", padding: 5, textAlign: "center" }}>
+          <Typography variant="h3">A little bit about me</Typography>
+          <Typography variant="subtitle1" sx={{ paddingTop: 2 }}>
+            I enjoy a workout and playing basketball. Sweating is the best way
+            to clear my head and energize me for the rest of the day. After
+            sweating and burning valuable calories, I love to regenerate myself
+            by devouring a bowl of ramen (my favorite comfort food). Outside of
+            the court, gym, and ramen restaurant. I would put my creativity to
+            work by working on different full-stack projects by creating react
+            web applications.
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ textAlign: "center" }}>
+        <ImageList sx={{ width: 400, height: 350 }} cols={2} rowHeight={164}>
+          {imagesData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={item.img}
+                srcSet={item.img}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Grid>
     </Grid>
   );
---
+++ src/components/ColorScheme.js
@@ -0,0 +1,4 @@
+export const backgroundColor1 = "#222831";
+export const backgroundColor2 = "#EEEEEE";
+export const textColor1 = "#00ADB5";
+export const textColor2 = "#393E46";
