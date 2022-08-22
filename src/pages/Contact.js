e7e92d0c842b1460a46a2aecdb61e0737a188542
tuanhoangle <tuanleprofessional@gmail.com>
2022-08-22 01:21:55 -0400

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,124 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useRef } from "react";
+import { Box, Grid, Typography, Link, TextField, Button } from "@mui/material";
+import { FadeIn } from "react-slide-fade-in";
+import emailjs from "@emailjs/browser";
+
+import { LinkedIn, Place, Email, GitHub } from "@mui/icons-material/";
+
+import {
+  backgroundColor1,
+  backgroundColor2,
+  textColor1,
+} from "../css/ColorScheme";
 
 export default function Contact() {
+  const form = useRef();
+  const iconSize = 100;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7dxy7fy",
+        "template_hn0vkch",
+        form.current,
+        "ajQO3mSzeRGSq_-s1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <Box id="Contact" sx={{ height: 300, backgroundColor: "lightblue" }}>
-      Contact
+    <Box id="Contact">
+      <Box
+        sx={{
+          backgroundColor: backgroundColor1,
+          color: textColor1,
+          textAlign: "center",
+          p: 6,
+        }}
+      >
+        <FadeIn from="top" delayInMilliseconds={200} triggerOffset={0}>
+          <Box>
+            <Typography variant="h4">Contact</Typography>
+            <Typography variant="h3">Let's Create a Connection</Typography>
+          </Box>
+          <Grid container spacing={2} mt={5}>
+            <Grid item md={3} xs={12}>
+              <Place sx={{ fontSize: iconSize }} />
+              <Typography variant="h5">Atlanta, Georgia</Typography>
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <LinkedIn sx={{ fontSize: iconSize }} />
+              <Typography variant="h5">Let's Connect</Typography>
+            </Grid>
+
+            <Grid item md={3} xs={12}>
+              <Email sx={{ fontSize: iconSize }} />
+              <Typography variant="subtitle1">
+                tuanleprofessional@gmail.com
+              </Typography>
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <Link
+                href="https://github.com/tuanle99"
+                target="_blank"
+                rel="noopener"
+                underline="none"
+                sx={{ color: textColor1 }}
+              >
+                <GitHub sx={{ fontSize: iconSize }} />
+
+                <Typography variant="subtitle1">
+                  Check out my project
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            xs={12}
+            sx={{
+              backgroundColor: backgroundColor2,
+              mt: 3,
+              borderRadius: 3,
+            }}
+          >
+            <Typography variant="h5" sx={{ pt: 2 }}>
+              Question? Leave me a message and I will get back to you as soon as
+              possible!
+            </Typography>
+            <form ref={form} onSubmit={sendEmail}>
+              <TextField
+                label="Name"
+                name="name"
+                sx={{ display: "flex", m: 2 }}
+              />
+              <TextField
+                label="Subject"
+                name="subject"
+                sx={{ display: "flex", m: 2 }}
+              />
+              <TextField
+                label="Message"
+                name="message"
+                sx={{ display: "flex", m: 2 }}
+              />
+              <Button type="submit" sx={{ m: 1 }}>
+                Submit
+              </Button>
+            </form>
+          </Grid>
+        </FadeIn>
+      </Box>
     </Box>
   );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,40 +1,20 @@
-import React, { useEffect, useRef, useState, useMemo } from "react";
+import React, { useRef } from "react";
 import { Box } from "@mui/material";
+
+import { useIsInViewport } from "../util/Helper";
 
 export default function Resume() {
   const ref1 = useRef(null);
 
   const isInViewport1 = useIsInViewport(ref1);
-  console.log("isInViewport1: ", isInViewport1);
+  // console.log("isInViewport1: ", isInViewport1);
   return (
     <Box
       ref={ref1}
-      id="Home"
+      id="Resume"
       sx={{ height: 300, backgroundColor: "lightblue" }}
     >
       Resume
     </Box>
   );
-}
-
-function useIsInViewport(ref) {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-
-  const observer = useMemo(
-    () =>
-      new IntersectionObserver(([entry]) =>
-        setIsIntersecting(entry.isIntersecting)
-      ),
-    []
-  );
-
-  useEffect(() => {
-    observer.observe(ref.current);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [ref, observer]);
-
-  return isIntersecting;
 }
